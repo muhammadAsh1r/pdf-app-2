fa6d425e07e1b7265cc3195cc5fe8b397fd9eb07
Muhammad Ashir Khan <just4fun1678@gmail.com>
2026-01-14 16:07:18 +0500

    Sipleified

--- saa-s-pdf-utility-ui/app/(public)/verify-email/success/page.tsx
+++ saa-s-pdf-utility-ui/app/(public)/verify-email/success/page.tsx
@@ -1,32 +1,9 @@
-// "use client";
-// export const dynamic = 'force-dynamic';
+import React from 'react'
 
-// import { useEffect } from "react";
-// import { useRouter, useSearchParams } from "next/navigation";
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
 
-// const API_BASE = process.env.NEXT_PUBLIC_API_URL!;
-
-// export default function VerifySuccessPage() {
-//   const router = useRouter();
-//   const params = useSearchParams();
-//   const token = params.get("token");
-
-//   useEffect(() => {
-//     if (!token) {
-//       router.replace("/login");
-//       return;
-//     }
-
-//     fetch(`${API_BASE}/api/auth/verify-email/?token=${token}`, {
-//       credentials: "include",
-//     })
-//       .then(() => {
-//         router.replace("/dashboard");
-//       })
-//       .catch(() => {
-//         router.replace("/verify-email");
-//       });
-//   }, [token, router]);
-
-//   return null;
-// }
+export default page
